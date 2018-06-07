64c26a35cf0edef49d9fbf280add1404ebcafd1b
Daniel Pimentel <you@example.com>
2018-06-07 13:25:38 -0400

    adding routes

--- src/routes/exercise.php
+++ src/routes/exercise.php
@@ -15,6 +15,7 @@ $app->get('/api/exercises', function(Request $request, Response $response){
     $sql = "SELECT * FROM exercises";
     try{
         // Get DB Object
+        echo 'hell';
         $db = new db();
         // Connect
         $db = $db->connect();
@@ -26,4 +27,38 @@ $app->get('/api/exercises', function(Request $request, Response $response){
         echo '{"error": {"text": '.$e->getMessage().'}';
     }
 });
+
+$app->get('/api/exercises2', function(Request $request, Response $response){
+    $sql = "SELECT * FROM exercises WHERE category = 2";
+    try{
+        // Get DB Object
+        $db = new db();
+        // Connect
+        $db = $db->connect();
+        $stmt = $db->query($sql);
+        $exercises = $stmt->fetchAll(PDO::FETCH_OBJ);
+        $db = null;
+        echo json_encode($exercises);
+    } catch(PDOException $e){
+        echo '{"error": {"text": '.$e->getMessage().'}';
+    }
+});
+
+$app->get('/api/exercises/3', function(Request $request, Response $response){
+    $sql = "SELECT * FROM exercises WHERE category = 3";
+    try{
+        // Get DB Object
+        $db = new db();
+        // Connect
+        $db = $db->connect();
+        $stmt = $db->query($sql);
+        $exercises = $stmt->fetchAll(PDO::FETCH_OBJ);
+        $db = null;
+        echo json_encode($exercises);
+    } catch(PDOException $e){
+        echo '{"error": {"text": '.$e->getMessage().'}';
+    }
+});
+
+
 $app->run();
